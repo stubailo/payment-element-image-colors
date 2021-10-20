961f97f987ea17737a278d9350d2e857f8b25eba
Sashko Stubailo <sashko@stripe.com>
2021-10-20 11:15:41 -0700

    Image uploading works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [imageData, setImageData] = useState("");
+
+  const validTypes = ["image/jpeg", "image/png", "image/gif"];
+
+  const handleDrop = useCallback((event) => {
+    event.preventDefault();
+
+    const files = event.dataTransfer.files;
+
+    if(!files.length) {
+      setErrorMessage("no files");
+      throw new Error("no files");
+    }
+
+    [...files].forEach((file) => {
+      if (!validTypes.includes(file.type)) {
+        setErrorMessage("not an image!");
+        throw new Error("not an image");
+      }
+
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        setImageData(reader.result);
+      }, false);
+
+      reader.readAsDataURL(file);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={imageData} className="App-logo" alt="logo" />
+        <p>Error: {errorMessage}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +47,20 @@ function App() {
         >
           Learn React
         </a>
+        <div
+          style={{
+            width: "200px",
+            height: "200px",
+            margin: "0 auto",
+            background: "green",
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+          }}
+          onDrop={handleDrop}
+        >
+          Drop image here
+        </div>
       </header>
     </div>
   );
